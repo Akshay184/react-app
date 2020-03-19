282f2fffc87835891c6cc2dcdddc77cb7e4a913e
akshay184 <akshayagarwal184@gmail.com>
2020-03-19 13:46:23 +0530

    creating owm component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className = "App">
-    //     <h1>Hi, I am a react App</h1>
-    //   </div>
-    // );
+    return (
+      <div className = "App">
+        <h1>Hi, I am a react App</h1>
+        <p>This is rally working!</p>
+        <Person /> 
+      </div>
+    );
     // above statement can also be written as below
-    return React.createElement('div', { className : 'App'}, React.createElement('h1', null, 'Does this work?'));
+    // return React.createElement('div', { className : 'App'}, React.createElement('h1', null, 'Does this work?'));
   }
 }
 
