3452ed75883e0b1b25552b442ec7800e6c03ce85
akshay184 <akshayagarwal184@gmail.com>
2020-03-21 00:44:18 +0530

    HOX AUX

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/aux';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>My name is {this.props.name} and age is {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.change} value={this.props.name} />
-            </div>
+            </Aux>
         )
     }
 }
